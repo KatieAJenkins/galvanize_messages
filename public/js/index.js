218e1ec25456a802e8cf2abe781c175ca3ef6a2d
KatieAJenkins <kind2karma@gmail.com>
2017-06-21 20:47:38 -0600

    delete functionality is working

--- public/js/index.js
+++ public/js/index.js
@@ -27,7 +27,6 @@ $('#submit').click(function (event){
     .fail(($xhr) => {
       console.log($xhr);
     });
-
 });
 
   $.getJSON('/messages') //this fires after page reloads -->this is the API endpoint
@@ -36,6 +35,10 @@ $('#submit').click(function (event){
 
       //Loop through results from getJSON call to dynamically add to page
       for (var i = 0; i < results.length; i++) {
+
+        var $id = results[i].id;
+        // console.log($id);
+
         var $message = $('<p>').text(results[i].message);
 
         //Create Edit and Delete buttons manually
@@ -45,18 +48,46 @@ $('#submit').click(function (event){
 
         var $deleteButton = document.createElement('button');
         var $deleteMessage = document.createTextNode('Delete');
+        $deleteButton.setAttribute("class", "deleteButton");
+        $deleteButton.setAttribute("id", $id);
         $deleteButton.appendChild($deleteMessage);
 
         var $name = $('<h6>').text(results[i].name);
+
         //Append all dynamically created DOM elements to message container
         $('#messageContainer').append($message);
         $('#messageContainer').append($name);
         $('#messageContainer').append($editButton);
         $('#messageContainer').append($deleteButton);
-      }
-    })
 
+////DELETE FUNCTIONALITY////
+  //  deleteMessage($id){
+        $('.deleteButton').click(function(event) {
+          event.preventDefault();
+          // console.log("clicked");
+          var thisMessage = $(this);
+          var thisMessageId = thisMessage[0].getAttribute('id');
+          console.log(thisMessageId);
+
+          const options = {
+            dataType: 'json',
+            type: 'DELETE',
+            url: `/messages/${thisMessageId}`
+          };
+
+          $.ajax(options)
+            .done(() => {
+              console.log('DELETED!');
+              window.location.href = '/'; //reloads page once .done is called
+            })
+            .fail(($xhr) => {
+              console.log($xhr);
+            });
+        });
+      }
+    // }
+  })
     .fail(() => {
-      $('#messageContainer').text('Could not get messages');
+      $('#messageContainer').text('Could not delete messages');
     });
 }); //document ready closure
